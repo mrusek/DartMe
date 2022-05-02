6aa8a107fbc27a4d68e2e0b324739787ea9a24fe
Mateusz Rusek <rusek.mateusz@poczta.pl>
2022-05-02 22:59:11 +0200

    First real progress - added newgame components without styling and hooking them yet

---
+++ DartMe/src/routes/__layout.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import Drawer, { AppContent, Content } from '@smui/drawer';
+	import List, { Item, Text } from '@smui/list';
+
+	let clicked = 'nothing yet';
+</script>
+
+<header />
+<div class="drawer-container">
+	<Drawer>
+		<Content>
+			<List>
+				<Item href="/newGame" on:click={() => (clicked = 'NewGame')}>
+					<Text>New Game</Text>
+				</Item>
+				<Item href="/loadGame" on:click={() => (clicked = 'LoadGame')}>
+					<Text>Load Game</Text>
+				</Item>
+				<Item href="/archive" on:click={() => (clicked = 'Archive')}>
+					<Text>Archive</Text>
+				</Item>
+				<Item href="/gameView" on:click={() => (clicked = 'GameView')}>
+					<Text>Game View</Text>
+				</Item>
+			</List>
+		</Content>
+	</Drawer>
+
+	<AppContent class="app-content">
+		<main class="main-content">
+			<slot />
+		</main>
+	</AppContent>
+</div>
+<footer>
+	<p>Copyright 2022 Mateusz Rusek</p>
+</footer>
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 20px auto;
+	}
+	footer {
+		text-align: center;
+		position: fixed;
+		bottom: 0;
+		z-index: 99;
+	}
+
+	.drawer-container {
+		display: flex;
+		overflow: hidden;
+		position: fixed;
+		z-index: 99;
+		width: 100vw;
+		height: 100vh;
+		top: 0;
+		bottom: 0;
+		left: 0;
+	}
+
+	* :global(.app-content) {
+		flex: auto;
+		overflow: auto;
+		position: relative;
+		flex-grow: 1;
+	}
+
+	.main-content {
+		overflow: auto;
+		height: 100vh;
+		box-sizing: border-box;
+	}
+</style>
---
+++ DartMe/src/routes/newGame.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import Textfield from '@smui/textfield';
+	import Checkbox from '@smui/checkbox';
+	import Icon from '@smui/textfield/icon';
+	import HelperText from '@smui/textfield/helper-text';
+	import Slider from '@smui/slider';
+	import Radio from '@smui/radio';
+	import FormField from '@smui/form-field';
+	import Button, { Label } from '@smui/button';
+
+	let newGameName: string = '';
+	let gameVariants: Array<string> = ['301', '501'];
+	let isRankedGame: boolean = true;
+	let selectedGameVariant: string = '';
+	let isStartGameDisabled: boolean = true;
+	let playerNumbers: number = 1;
+	let players: Array<string> = Array<string>(playerNumbers);
+</script>
+
+<div>
+	<div>
+		<Textfield class="shaped-filled" variant="filled" bind:value={newGameName} label="Nazwa gry" />
+		<div class="radio-demo">
+			{#each gameVariants as option}
+				<FormField>
+					<Radio bind:group={selectedGameVariant} value={option} touch />
+					<span slot="label">{option}</span>
+				</FormField>
+			{/each}
+		</div>
+		<FormField>
+			<Checkbox bind:isRankedGame touch />
+			<span slot="label">Gra rankingowa</span>
+		</FormField>
+		<FormField align="end" style="display:flex">
+			<Slider
+			style="flex-grow: 1;"
+			bind:value={playerNumbers}
+			min={1}
+			max={12}
+			step={1}
+			discrete
+			tickMarks
+			input$aria-label="Liczba graczy"
+		/>
+		<span slot="label">Liczba graczy</span>
+		</FormField>
+		{#each Array(playerNumbers) as _, index}
+			<Textfield
+				class="shaped-filled"
+				variant="filled"
+				bind:value={newGameName}
+				label={`Gracz numer ${index+1}`}
+			/>
+		{/each}
+		<Button on:click={() => {}} bind:disabled={isStartGameDisabled} touch variant="unelevated">
+			<Label>Rozpocznij grę!</Label>
+		</Button>
+	</div>
+</div>
+
+<style>
+	* :global(.shaped-filled) {
+		border-radius: 16px 16px 0 0;
+	}
+	.radio-demo > :global(*) {
+		margin: 0 0.2em;
+	}
+</style>
